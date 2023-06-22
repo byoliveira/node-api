51899eebbe5e8e3154c659b3ca7608a2618b1818
Bruno Yuri <bruno.yuri.alves.oliveira@outlook.com>
2023-06-21 22:02:29 -0300

    primeiro commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	dialect: 'mysql',
+	host: '127.0.0.1',
+	username: 'root',
+	password: 'r10314jc',
+	database: 'gama',
+		define: {
+			timestamps: true, 
+			underscored: true,
+			underscoredAll:true,
+	},
+};
